1fba89ea934469d92f3d15673102bb1b76799886
Alvaro245ap <97471984+Alvaro245ap@users.noreply.github.com>
2025-10-20 00:04:05 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,8 @@
-const CACHE="rehabit-ux-v2";
+const CACHE="rehabit-vivid-v3";
 const ASSETS=[
   "./","./index.html","./styles.css","./app.js","./manifest.webmanifest",
   "./assets/icon-192.png","./assets/icon-512.png","./assets/favicon.png",
-  "./assets/tech.png","./assets/smoking.png","./assets/alcohol.png","./assets/gambling.png",
-  "./assets/encourage.jpg"
+  "./assets/tech.png","./assets/smoking.png","./assets/alcohol.png","./assets/gambling.png"
 ];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))).then(()=>self.clients.claim()))});
